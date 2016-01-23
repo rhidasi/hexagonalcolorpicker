b4d4c97d43def0f1e96b92e86bba0166032939ae
rhidasi <robert.hidasi@gmail.com>
2016-01-23 22:13:53 +0100

    Revert "Code cleaning" HexagonalColorPickerPreference.java

--- hexagonalColorPicker/src/main/java/sk/hidasi/hexagonalcolorpicker/HexagonalColorPickerPreference.java
+++ hexagonalColorPicker/src/main/java/sk/hidasi/hexagonalcolorpicker/HexagonalColorPickerPreference.java
@@ -37,15 +37,21 @@ public class HexagonalColorPickerPreference extends Preference implements OnColo
 	private int mValue;
 
 	public HexagonalColorPickerPreference(Context context) {
-		this(context, null, 0);
+		super(context);
+		initAttrs(null, 0);
 	}
 
 	public HexagonalColorPickerPreference(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
+		super(context, attrs);
+		initAttrs(attrs, 0);
 	}
 
 	public HexagonalColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+		initAttrs(attrs, defStyle);
+	}
+
+	private void initAttrs(AttributeSet attrs, int defStyle) {
 
 		final TypedArray a = getContext().getTheme().obtainStyledAttributes(
 				attrs, R.styleable.HexagonalColorPicker, defStyle, defStyle);
